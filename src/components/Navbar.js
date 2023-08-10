885b1d19ef21285e8edfc601413fbec57481964d
imnotkate <k.rankin07@outlook.com>
2023-08-11 00:52:39 +0100

    navbar updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,9 +26,9 @@ function Navbar() {
     <>
     <nav className='navbar'>
         <div className='navbar-container'>
-            <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            The Balugas
-            </Link>
+            
+            <img className='logo_image' src='images/TheBalugas2.jpg' alt='The Balugas'/>
+            
             <div className='menu-icon' onClick={handleClick}>
                 <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
             </div>

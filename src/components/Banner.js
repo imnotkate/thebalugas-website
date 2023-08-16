4f1491be98e568b90809d10b69fb4d8d6aeaa579
imnotkate <k.rankin07@outlook.com>
2023-08-16 23:53:07 +0100

    home section start

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import '../App.css'
+import { Button } from './Button'
+import './Banner.css'
 
 function Banner() {
   return (
-    <div>
-
+    <div className='banner-container'>
+        <h1>The Balugas</h1>
+        <h2>New Single</h2>
+        <h2>"One Night"</h2>
+    <div className='banner-btn'>
+        <Button className="btn" buttonStyles='btn--outline' buttonSize='btn--large'>Stream</Button>
+    </div>
     </div>
   )
 }
---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../App.css'
+import Banner from '../Banner'
+
+function Home() {
+    return (
+        <>
+        <Banner />
+        </>
+    );
+}
+
+export default Home;
